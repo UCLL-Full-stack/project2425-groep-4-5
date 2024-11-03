c9e747f66a797a0948c42bde21ef538fe55724cc
Egor-Kolomiets <95211298+Not-Igor@users.noreply.github.com>
2024-11-04 00:02:23 +0100

    route voor doctor

--- back-end/controller/doctor.routes.ts
+++ back-end/controller/doctor.routes.ts
@@ -1,8 +1,57 @@
+/**
+ * @swagger
+ *   components:
+ *    securitySchemes:
+ *     bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
+ *    schemas:
+ *      Doctor:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: number
+ *              format: int64
+ *            name:
+ *              type: string
+ *              description: Lecturer name.
+ *            empail:
+ *              type: string
+ *              description: Email.
+ *            specialisation:
+ *              type: string
+ *              description: Specialisation.
+ *            description:
+ *              type: string
+ *              description: Description.
+ *            clinic:
+ *              $ref: '#/components/schemas/Clinic' 
+ *            user:
+ *              $ref: '#/components/schemas/User'
+ */
+
 import express from "express";
 import doctorService from "../service/doctor.service";
 import { NextFunction, Request, Response } from "express";
 
 const doctorRouter = express.Router();
+
+/**
+ * @swagger
+ * /doctors:
+ *   get:
+ *     summary: Get a list of all doctors.
+ *     responses:
+ *       200:
+ *         description: A list of doctors.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/Doctors'
+ */
 
 doctorRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
